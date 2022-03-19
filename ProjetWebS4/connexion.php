1e4e51053a0749ddc8c89d61e6beacb2763a3970
ThomasFAZZARI <76949783+ThomasFAZZARI@users.noreply.github.com>
2022-03-19 15:55:03 +0100

    Add files via upload

--- ProjetWebS4/connexion.php
+++ ProjetWebS4/connexion.php
@@ -7,7 +7,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-	<title>Inscription</title>
+	<title>Se connecter</title>
 </head>
 <header>
 
@@ -19,26 +19,26 @@
         		<a class="nav-link" href="inscription.php">S'inscrire</a>
       		</li>
       		<li class="nav-item active">
-        		<a class="nav-link" href="Connexion.php">Se connecter</a>
+        		<a class="nav-link" href="connexion.php">Se connecter</a>
       		</li>
   		</div>
 	</nav>
 
 </header>
 <body>
-	<?php 
-		$connexion = mysqli_connect("localhost" , "root" , "") or die ("Impossible de se connecter : " . mysqli_error());
-		mysqli_select_db($connexion, "ProjetWebS4");
-	 ?>
+
 	 <br><br>
-	 
-	 <form class="form-horizontal" method="POST" action="scripts/scriptInscription.php" style="width:500px;margin:auto;">
- 		<div class="form-group">
-    		<label for="pseudo" class="col-sm-2 control-label">Pseudo</label>
-    		<div class="col-sm-10">
-     			<input type="text" class="form-control" required id="nom" name="nom" placeholder="Votre pseudo">
-   			</div>
- 		</div>
+	 <?php
+	 	if(isset($_GET['erreur']))
+		{
+			$err = $_GET['erreur'];
+			if($err==1 || $err==2)
+			{
+				echo "<p style='color:red'> E-mail ou mot de passe incorrect </p>";
+			}
+		}
+	?>
+	 <form class="form-horizontal" method="POST" action="scripts/scriptConnexion.php" style="width:500px;margin:auto;">
  		<div class="form-group">
     		<label for="mail" class="col-sm-2 control-label">E-mail</label>
     		<div class="col-sm-10">
@@ -53,19 +53,12 @@
 		</div>
 		<div class="form-group">
     		<div class="col-sm-offset-2 col-sm-10">
-				<div class="checkbox">
-					<label>
-						 <input type="checkbox" name="organisateur"> Êtes-vous organisateur ?
-					</label>
-				</div>
-			</div>
-		</div>
-		<div class="form-group">
-    		<div class="col-sm-offset-2 col-sm-10">
-				<input type="submit" name="submit" class="btn btn-dark" value="Valider">
+				<input type="submit" name="submit" class="btn btn-dark" value="Se connecter">
 			</div>
 		</div>
 	</form> 
-	
+
+
+
 </body>
 </html>
---
+++ ProjetWebS4/electeur.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+	<title>Accueil</title>
+</head>
+<header>
+	<?php
+		session_start();
+	?>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+			<a class="navbar-brand" href="electeur.php">Accueil</a>
+    		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      		<li class=s"nav-item active">
+        		<a class="nav-link" href="scripts/scriptDeconnexion.php">Se déconnecter</a>
+      		</li>
+      		<li class="nav-item active">
+        		<a class="nav-link" href="modifierCompte.php">Modifier son compte</a>
+      		</li>
+  		</div>
+	</nav>
+</header>
+<body>
+
+	<?php echo '<div style="text-align: center;"><br><h1 class="h1">Bienvenue, '.$_SESSION["pseudo"].'</h1><br></div>'; ?>
+	<div style="text-align: center;"><br><h1 class="h1">ÉLECTIONS EN COURS :</h1><br></div>
+
+	<div class="card-group">
+
+  		<div class="card">
+
+    	</div>
+
+	</div>
+
+</body>
+</html>
