8705e9d689597446357fa7baebaa26fdf3214da6
Nadège <fraselle.nadege@gmail.com>
2022-05-12 18:12:18 +0200

    nettoyage script création bdd

--- ProjetWebS4/ProjetWebS4.sql
+++ ProjetWebS4/ProjetWebS4.sql
@@ -1,26 +1,37 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost
--- Généré le : jeu. 05 mai 2022 à 14:19
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 7.4.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `ProjetWebS4`
 --
 DROP DATABASE  IF EXISTS `ProjetWebS4`;
 CREATE DATABASE IF NOT EXISTS `ProjetWebS4`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+CREATE TABLE `Utilisateur` (
+  `IdUtilisateur`int(10) NOT NULL AUTO_INCREMENT,
+  `Pseudo` varchar(20) NOT NULL,
+  `Mail` varchar(50) NOT NULL,
+  `MotDePasse` varchar(96) NOT NULL,
+  `EstOrganisateur` tinyint(1) DEFAULT NULL,
+  `Sel` varchar(96) NULL,
+  PRIMARY KEY (IdUtilisateur)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`Pseudo`, `Mail`, `MotDePasse`, `EstOrganisateur`, `Sel`) VALUES
+('testPseudo', 'testMail', 'testMDP', 1, 'aa'),
+('pseudo', 'mail@m.com', 'kuhgiu', 0, 'aa'),
+('kjbbk', 'noui@uiggiu.com', 'iubiuuih', 1, 'aa'),
+('pseudo2', 'mail@mail.fr', 'mdp', 0, 'aa'),
+('thomas', 'thomas@mail.fr', 'thomas', 1, 'aa');
+
 -- --------------------------------------------------------
 
 --
@@ -28,12 +39,13 @@ CREATE DATABASE IF NOT EXISTS `ProjetWebS4`;
 --
 
 CREATE TABLE `Destination` (
-  `IdDestination` int(11) NOT NULL,
+  `IdDestination` int(11) NOT NULL AUTO_INCREMENT,
   `Nom` varchar(25) NOT NULL,
   `Pays` varchar(25) NOT NULL,
   `TempMoyenne` int(2) NOT NULL,
   `Description` varchar(100) NOT NULL,
-  `Image` varchar(50) NOT NULL
+  `Image` varchar(50) NOT NULL,
+   PRIMARY KEY (IdDestination)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -63,23 +75,25 @@ INSERT INTO `Destination` (`IdDestination`, `Nom`, `Pays`, `TempMoyenne`, `Descr
 --
 
 CREATE TABLE `Election` (
-  `IdElection` int(10) NOT NULL,
+  `IdElection` int(10) NOT NULL AUTO_INCREMENT,
   `Intitule` varchar(50) NOT NULL,
   `estTerminee` tinyint(4) NOT NULL,
-  `IdOrga` int(10) NOT NULL
+  `IdOrga` int(10) NOT NULL,
+  PRIMARY KEY (IdElection),
+  FOREIGN KEY (`IdOrga`) REFERENCES `Utilisateur` (`IdUtilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `Election`
 --
 
-INSERT INTO `Election` (`IdElection`, `Intitule`, `estTerminee`) VALUES
-(35, 'Villes du monde', 0),
-(61, 'Villes de France', 0),
-(64, 'test', 1),
-(65, 'Villes du monde.ji', 0),
-(66, 'iholhlhhléjl', 0),
-(67, 'ékjjlljléélnlkl', 1);
+INSERT INTO `Election` (`IdElection`, `Intitule`, `estTerminee`, `IdOrga`) VALUES
+(35, 'Villes du monde', 0, 1),
+(61, 'Villes de France', 0, 1),
+(64, 'test', 1, 1),
+(65, 'Villes du monde.ji', 0, 2),
+(66, 'iholhlhhléjl', 0, 3),
+(67, 'ékjjlljléélnlkl', 1, 4);
 
 -- --------------------------------------------------------
 
@@ -90,7 +104,11 @@ INSERT INTO `Election` (`IdElection`, `Intitule`, `estTerminee`) VALUES
 CREATE TABLE `Participation` (
   `IdElection` int(50) NOT NULL,
   `IdDestination` int(50) NOT NULL,
-  `nbVotes` int(50) NOT NULL
+  `nbVotes` int(50) NOT NULL,
+  PRIMARY KEY (`IdElection`,`IdDestination`),
+  KEY `fk_destination` (`IdDestination`),
+  FOREIGN KEY (`IdDestination`) REFERENCES `Destination` (`IdDestination`),
+  FOREIGN KEY (`IdElection`) REFERENCES `Election` (`IdElection`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -110,95 +128,14 @@ INSERT INTO `Participation` (`IdElection`, `IdDestination`, `nbVotes`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Utilisateur`
---
-
-CREATE TABLE `Utilisateur` (
-  `IdUtilisateur`int(10) NOT NULL,
-  `Pseudo` varchar(20) NOT NULL,
-  `Mail` varchar(50) NOT NULL,
-  `MotDePasse` varchar(96) NOT NULL,
-  `EstOrganisateur` tinyint(1) DEFAULT NULL,
-  `Sel` varchar(96) NULL,
-  PRIMARY KEY (Pseudo,IdUtilisateur)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `Utilisateur`
---
-
-INSERT INTO `Utilisateur` (`Pseudo`, `Mail`, `MotDePasse`, `EstOrganisateur`, `Sel`) VALUES
-('testPseudo', 'testMail', 'testMDP', 1, 'aa'),
-('pseudo', 'mail@m.com', 'kuhgiu', 0, 'aa'),
-('kjbbk', 'noui@uiggiu.com', 'iubiuuih', 1, 'aa'),
-('pseudo2', 'mail@mail.fr', 'mdp', 0, 'aa'),
-('thomas', 'thomas@mail.fr', 'thomas', 1, 'aa');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `commentaires`
 -- 
 CREATE TABLE `commentaires` ( 
-  `IdCommentaire` INT(10) NOT NULL AUTO_INCREMENT , 
+  `IdCommentaire` INT(10) NOT NULL AUTO_INCREMENT, 
   `IdElection` INT(10) NOT NULL , 
   `pseudo` VARCHAR(20) NOT NULL , 
   `message` TEXT NOT NULL , 
-  `dateMsg` VARCHAR(30) NOT NULL , 
+  `dateMsg` VARCHAR(30) NOT NULL ,
   PRIMARY KEY (`IdCommentaire`)
   ); 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `Destination`
---
-ALTER TABLE `Destination`
-  ADD PRIMARY KEY (`IdDestination`);
-
---
--- Index pour la table `Election`
---
-ALTER TABLE `Election`
-  ADD PRIMARY KEY (`IdElection`);
-
---
--- Index pour la table `Participation`
---
-ALTER TABLE `Participation`
-  ADD PRIMARY KEY (`IdElection`,`IdDestination`),
-  ADD KEY `fk_destination` (`IdDestination`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `Destination`
---
-ALTER TABLE `Destination`
-  MODIFY `IdDestination` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT pour la table `Election`
---
-ALTER TABLE `Election`
-  MODIFY `IdElection` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
-  ADD CONSTRAINT `fk_orga` FOREIGN KEY (`IdOrga`) REFERENCES `Utilisateur` (`IdUtilisateur`);
---
--- Contraintes pour les tables déchargées
---
-ALTER TABLE `utilisateur` CHANGE `IdUtilisateur` `IdUtilisateur` INT(10) NOT NULL AUTO_INCREMENT; 
-
---
--- Contraintes pour la table `Participation`
---
-ALTER TABLE `Participation`
-  ADD CONSTRAINT `fk_destination` FOREIGN KEY (`IdDestination`) REFERENCES `Destination` (`IdDestination`),
-  ADD CONSTRAINT `fk_election` FOREIGN KEY (`IdElection`) REFERENCES `Election` (`IdElection`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
